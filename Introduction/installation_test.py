272d1d83d69d6ec5c9c65f11183bfc7d12ee927c
Rolando Borja <borja21rb@gmail.com>
2021-05-11 13:25:56 -0500

    Prueba de instalación de TF

---
+++ Introduction/installation_test.py
@@ -0,0 +1,13 @@
+# import TensorFlow 
+import tensorflow as tf 
+
+sess = tf.Session()
+
+hello= tf.constant("Hello :, RBB ")
+print(sess.run(hello))
+
+
+a = tf.constant(20)
+b = tf.constant(21)
+
+print(' a + b = {0}'.format(sess.run(a + b)))

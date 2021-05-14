4103f8edebba932edd463f5de39754bc6e7bb865
Rolando Borja <borja21rb@gmail.com>
2021-05-14 14:57:18 -0500

    Agrego archivo con implementación de Transfer Learning

--- Transfer_Learning/retrain.py
+++ Transfer_Learning/retrain.py
@@ -72,7 +72,7 @@ from tensorflow.python.util import compat
 FLAGS = None
 
 # These are all parameters that are tied to the particular model architecture
-# we're using for Inception v3. These include things like tensor names and their
+# we're using for Inception v3. These include things like tensor names and their.
 # sizes. If you want to adapt this script to work with another model, you will.
 # need to update these to reflect the values in the network you're using.
 # pylint: disable=line-too-long

6905a33bf5ef87fd6aa983a6e070d75f9be324c9
Rolando Borja <borja21rb@gmail.com>
2021-05-17 12:18:34 -0500

    Cambios menores

--- Keras/Keras_DeepMNIST.py
+++ Keras/Keras_DeepMNIST.py
@@ -5,3 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras import backend as K 
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
